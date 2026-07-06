9ac1b965b7faa59e376332084a65602e329d13d8
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2026-07-06 17:51:29 -0300

    tweak intro page layout as well

--- src/ui/screens/intro.rs
+++ src/ui/screens/intro.rs
@@ -3,7 +3,7 @@
 use ratatui::Frame;
 use ratatui::layout::{Constraint, Layout};
 use ratatui::style::Stylize;
-use ratatui::text::{Line, Text};
+use ratatui::text::{Line, Span, Text};
 use ratatui::widgets::{Block, Paragraph, Wrap};
 
 use crate::contants::APP_TITLE;
@@ -11,14 +11,18 @@ use crate::contants::APP_TITLE;
 pub(super) fn render(frame: &mut Frame) {
     let [
         title_bar_layout,
+        _,
         page_title_and_sub_layout,
         intro_text_layout,
+        _,
         status_layout,
     ] = Layout::vertical([
-        Constraint::Length(2), // title bar,
-        Constraint::Fill(2),   // page_title_and_sub
-        Constraint::Fill(3),   // intro_text_layout
-        Constraint::Length(3), // status bar
+        Constraint::Length(2),  // title bar,
+        Constraint::Fill(1),    // empty
+        Constraint::Fill(1),    // page_title_and_sub
+        Constraint::Length(15), // intro_text_layout
+        Constraint::Fill(1),    // empty
+        Constraint::Length(3),  // status bar
     ])
     .areas(frame.area());
 
@@ -39,24 +43,29 @@ pub(super) fn render(frame: &mut Frame) {
     );
 
     let intro_texts = Text::from(vec![
-        Line::from("ATENÇÃO".bold()),
+        Line::from("A T E N Ç Ã O".red().bold()),
         Line::from(""), // blank row for breathing space
         Line::from("Você está prestes a abrir uma porta para o desconhecido."),
         Line::from(""),
-        Line::from("Aconselho acender uma vela e apagar as luzes antes de executar o programa."),
+        Line::from("Aconselho acender uma vela e apagar as luzes antes de executar."),
         Line::from(""),
-        Line::from(
-            "Para que SUED responda, você deve elogiá-lo e em seguida perguntar de forma clara.",
-        ),
+        Line::from(vec![
+            Span::raw("Para que "),
+            Span::raw("SUED ").red().bold(),
+            Span::raw("responda, você deve elogiá-lo e em seguida pergunte com clareza."),
+        ]),
         Line::from(""),
         Line::from("Pessoas fracas e sensíveis não devem utilizar o programa."),
         Line::from(""),
         Line::from("Tenha muito cuidado com o que você irá perguntar..."),
+        Line::from(""),
+        Line::from(""),
+        Line::from("   CONTINUAR ▸   ".black().on_red().bold()),
     ]);
 
     frame.render_widget(
         Paragraph::new(intro_texts)
-            .red()
+            .white()
             .centered()
             .wrap(Wrap { trim: false }),
         intro_text_layout.centered_horizontally(Constraint::Percentage(50)),
