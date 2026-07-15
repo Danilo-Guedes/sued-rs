2bb330f93ea770484bddb652a7ca104b31f72a4a
Danilo Paz Guedes de Freitas <danilotiba@gmail.com>
2026-07-15 18:51:40 -0300

    fix config tests

--- src/language.rs
+++ src/language.rs
@@ -1,5 +1,9 @@
-#[derive(Debug, Clone, Copy, PartialEq)]
-pub enum LanguageOptions {
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize, Default)]
+#[serde(rename_all = "lowercase")]
+pub enum Language {
+    #[default]
     PtBr,
     EnUs,
     EsEs,
